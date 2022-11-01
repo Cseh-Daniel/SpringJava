0066c7d0c8a40180bcad564820362243a22c3681
Cseh Dániel <codaryf@gmail.com>
2022-11-01 17:54:54 +0100

    Project Initialization
    MoziMusor Project létrehozva
    A céget bemutató oldal előkészítése
    A bemutató oldalhoz szükséges Controller létrehozva
    Shared könyvtár - az összes oldalhoz szükséges kiegészítő fájlok (css, navbar, js, stb)
    
    Co-Authored-By: Polyak_Szabolcs <116500188+PolyakSzabolcs@users.noreply.github.com>

---
+++ MoziMusor/src/main/java/com/example/mozimusor/MoziMusorApplication.java
@@ -0,0 +1,13 @@
+package com.example.mozimusor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoziMusorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoziMusorApplication.class, args);
+    }
+
+}
